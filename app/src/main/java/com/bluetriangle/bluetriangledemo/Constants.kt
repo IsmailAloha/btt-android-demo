55299b4ab63c51ebc20fad1a1f2652814e2ee5d1
Erik Gallegos <erikgallegos25@gmail.com>
2021-10-31 16:40:36 -0500

    added input fields ui

---
+++ app/src/main/java/com/bluetriangle/bluetriangledemo/Constants.kt
@@ -0,0 +1,3 @@
+package com.bluetriangle.bluetriangledemo
+
+const val SITE_ID = "bluetriangledemo500z"
--- app/src/main/java/com/bluetriangle/bluetriangledemo/MainActivity.kt
+++ app/src/main/java/com/bluetriangle/bluetriangledemo/MainActivity.kt
@@ -2,10 +2,23 @@ package com.bluetriangle.bluetriangledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bluetriangle.analytics.Tracker
+import com.bluetriangle.bluetriangledemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fields = Tracker.getInstance().globalFields
+        with(binding) {
+            siteIdEditText.setText(SITE_ID)
+            globalUserIdEditText.setText(fields["gID"])
+            sessionIdEditText.setText(fields["sID"])
+        }
     }
 }

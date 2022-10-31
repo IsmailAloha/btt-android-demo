c3d67912ad8bc8251a09c316f063f8904d30298b
Jason Emerick <jemerick@gmail.com>
2022-10-31 18:08:10 -0400

    add Parcelize

--- app/src/main/java/com/bluetriangle/bluetriangledemo/data/Product.kt
+++ app/src/main/java/com/bluetriangle/bluetriangledemo/data/Product.kt
@@ -1,9 +1,13 @@
 package com.bluetriangle.bluetriangledemo.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Product(
     val id: Long,
     val name: String,
     val description: String,
     val image: String,
     val price: Double,
-)
+) : Parcelable

bed4460e640474b832ad9fb453d078b1e5e541bd
Jason Emerick <jemerick@gmail.com>
2022-10-31 15:23:54 -0400

    add Product data model

---
+++ app/src/main/java/com/bluetriangle/bluetriangledemo/data/Product.kt
@@ -0,0 +1,9 @@
+package com.bluetriangle.bluetriangledemo.data
+
+data class Product(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val image: String,
+    val price: Double,
+)
